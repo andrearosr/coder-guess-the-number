a36ed18fc9775de7867fcc33127831f1c25b8ea7
Andrea Rosales <andrearosr@gmail.com>
2021-06-29 18:11:01 -0500

    Set baseline

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import Colors from '../constants/colors';
 
 const Header = ({ title }) => {
   return (
     <View style={styles.header}>
       <Text style={styles.headerTitle}>{title}</Text>
-      <Image source={require('../assets/images/B.jpeg')} style={styles.image} />
     </View>
   )
 }
